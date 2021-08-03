f30475337f19ce866331bfe6c477240dcdb44b70
Joao Vanelli <vanellijoao@gmail.com>
2021-08-03 17:53:46 -0300

    Desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,10 @@
+CREATE VIEW top_3_artistas AS
+    SELECT 
+        a.artist AS artista, COUNT(*) AS seguidores
+    FROM
+        SpotifyClone.followed_artists AS fa
+            INNER JOIN
+        SpotifyClone.artists AS a ON fa.artist_id = a.artist_id
+    GROUP BY artista
+    ORDER BY seguidores DESC , artista DESC
+    LIMIT 3;
