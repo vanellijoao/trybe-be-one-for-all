425fcab3f15c57ea92512de044107c5edcc08321
Joao Vanelli <vanellijoao@gmail.com>
2021-08-04 14:46:59 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,13 @@
-SELECT 
-    s.song AS nome, COUNT(sh.song_id) AS reproducoes
-FROM
-    SpotifyClone.songs AS s
-        INNER JOIN
-    SpotifyClone.song_history AS sh ON s.song_id = sh.song_id
-        INNER JOIN
-    SpotifyClone.users AS u ON u.user_id = sh.user_id
-WHERE
-    u.plan_id IN (2 , 3)
-GROUP BY nome
-ORDER BY nome;
+CREATE VIEW cancoes_premium AS
+    SELECT 
+        s.song AS nome, COUNT(sh.song_id) AS reproducoes
+    FROM
+        SpotifyClone.songs AS s
+            INNER JOIN
+        SpotifyClone.song_history AS sh ON s.song_id = sh.song_id
+            INNER JOIN
+        SpotifyClone.users AS u ON u.user_id = sh.user_id
+    WHERE
+        u.plan_id IN (2 , 3)
+    GROUP BY nome
+    ORDER BY nome;
